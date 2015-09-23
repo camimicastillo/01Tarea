d34edbe83dca2672ede397bef60a85797ba55b55
Camila Castillo <camicastillopinto@gmail.com>
2015-09-23 18:44:12 -0300

    Parte 4.
    Se calcularon los tiempos de ejecucion de los algoritmos. Con lo que se
    finaliza la tarea.

--- codigo.py
+++ codigo.py
@@ -11,6 +11,7 @@ import numpy as np
 from astropy import constants as cons
 from astropy import units as u
 from scipy import integrate as integrate
+import time
 
 #Lectura del archivo que contiene el espectro del Sol
 D = np.loadtxt("sun_AM0.dat")
@@ -36,6 +37,9 @@ plt.savefig('Figura.png', bbox_inches='tight')
 plt.show()
 
 
+#Determinar tiempo de ejecucion Algoritmo Trapecio Propio
+a= time.time()
+
 #Integracion del espectro en longitud de onda usando el metodo del trapecio
 #"sum" corresponde al valor de la integral del espectro (Constante solar)
 #y "Ls" al valor de la Luminosidad total del sol
@@ -46,12 +50,17 @@ for i in range(1696):
 print ('Integral del espectro del Sol: CONSTANTE SOLAR')
 print (Ks)
 
+
+b= time.time() - a
+
 r= cons.au #Distancia entre el Sol y la Tierra en m
 rcm= r.to('cm')
 Ls= 4*np.pi*(rcm*rcm)*Ks
 print ('Luminosidad total del Sol')
 print (Ls)
 
+#Calculo del tiempo de ejecucion del Metodo de Simpson propio
+c=time.time()
 #Integracion de la funcion de Planck
 #A partir de la ecuacion dada en la tarea, se realiza el cambio de
 #variable y= arctan(x). Ahora calcularemos esta integral con el metodo de
@@ -112,6 +121,8 @@ planck= cplanck * Sfinal
 print ('Valor de integral de funcion de Planck: integral calculada * constantes . FLUJO DE ENERGIA ')
 print(planck)
 
+d=time.time() - c
+
 #Conversion de la constante solar a J/m^2 * s
 KsJ=Ks.to('J / (m2 s)')
 
@@ -123,17 +134,29 @@ print (rSOL)
 
 
 #Recalcular las integrales anteriores con los metodos predeterminados de python
+
+#Valor del tiempo de ejecucion para trapz
+e= time.time()
+
 #Calculo integral del espectro con metodo de trapecio predeterminado
 esp_trapz= np.trapz(y_cgs, x_um)
 print ('Valor integral del espectro con metodo trapecio predeterminado')
 print (esp_trapz)
 
+f= time.time() - e
+
+#Tiempo de ejecucion trapz
+g= time.time()
 #Calculo de integral de funcion de Planck con metodo de trapecio predeterminado
 pln_trapz= np.trapz(y_pl, x_pl)
 plntrapz= pln_trapz * cplanck
 print ('Valor integral de Planck con metodo trapecio predeterminado')
 print (plntrapz)
 
+h= time.time() - g
+
+#Tiempo de ejecucion quad
+l= time.time()
 #Calculo de la integral de funcion de Planck con quad
 fu= lambda x: (x**3)/(np.exp(x) - 1)
 pln_quad= integrate.quad( fu , 0, np.inf)
@@ -145,4 +168,16 @@ plnquad= pln_quad1 * cplanck
 print ('Valor de la integral de la funcion de Planck con metodo predeterminado quad')
 print (plnquad)
 
+m= time.time() - l
+
 #Calculo del tiempo de ejecucion
+print ('Tiempo de ejecucion algoritmo propio de Trapecio para Integral del Espectro Solar')
+print (b)
+print ('Tiempo de ejecucion algoritmo propio Simpson para Integral Funcion de Planck ')
+print (d)
+print ('Tiempo de ejecucion trapz para Integral del Espectro Solar')
+print (f)
+print ('Tiempo de ejecucion trapz para Integral Funcion de Planck')
+print (h)
+print ('Tiempo de ejecucion quad para Integral Funcion de Planck')
+print (m)
